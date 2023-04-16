da6e19ccd73af4fe0cdcd26e918db7e4bfb5fe2a
Saif Alhaider <saifharith90@gmail.com>
2023-04-17 02:03:12 +0300

    add:created time to clothes model

--- app/src/main/java/com/example/cozy_closet/models/Clothes.kt
+++ app/src/main/java/com/example/cozy_closet/models/Clothes.kt
@@ -1,6 +1,12 @@
 package com.example.cozy_closet.models
 
+import java.time.LocalDateTime
+
 data class Clothes(
-    val dress:Int,
-    val pants:Int
-){}
+    val dress: Int,
+    val pants: Int,
+    val _timeCreated: LocalDateTime = LocalDateTime.now()
+) {
+    val timeCreated: LocalDateTime
+        get() = _timeCreated
+}
